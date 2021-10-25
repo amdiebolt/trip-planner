900240490ef46e3e2dd6e68b79f8d58970325645
Alex Diebolt <alexander.diebolt@gmail.com>
2021-10-25 13:02:08 -0700

    linking html pages and adding parameters to url

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-var buttonEl = document.querySelector('.button')
+var buttonEl = document.querySelector('#button')
+var inputEl = document.querySelector('#inputValue')
+var city 
 
 
-buttonEl.addEventListener('click', getRepo())
-function getRepo(event){
+// var formSubmitHandler = function (event) {
+//      event.preventDefault()
+  
+//     city = inputEl.value.trim()
+//      if (city) {
+//       getRepo(city)
+      
+
+//     } else {
+//       alert('Please enter a city')
+//          }
+//  }
+
+
+ function pageChange(event) {
     event.preventDefault()
-    location.assign('\landing.html')
-}
+    city = inputEl.value
+  
+     if (!inputEl) {
+      console.error('You need to search for a city!');
+       return;
+     }
+  
+    var queryString = './landing.html?q=' + city
+  
+    location.assign(queryString)
+    
+  }
+  buttonEl.addEventListener('click', pageChange)
+
+
+
+
+
 
