6e85dbcd0ac38df6439fd870bc20d477ae9a157f
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2021-10-25 16:48:54 -0700

    started fetch outline
    created variables for root URLs and API keys. changed some placement of ids. Commented out conflicting code to talk over with classmates to see where we wish to go . Can easily combine both codes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var inputEl = document.querySelector('input[name="searchCity"]')
+var inputEl = document.querySelector('input[name="cityName"]')
 
 
 
@@ -23,9 +23,9 @@ function getRepo(event){
     location.assign('./landing.html')
 
 }
-var buttonEl = document.querySelector('#button')
-var inputEl = document.querySelector('#inputValue')
-var city 
+// var buttonEl = document.querySelector('#button')
+// var inputEl = document.querySelector('#inputValue')
+// var city 
 
 
 // var formSubmitHandler = function (event) {
@@ -42,25 +42,25 @@ var city
 //  }
 
 
- function pageChange(event) {
-    event.preventDefault()
-    city = inputEl.value
+//  function pageChange(event) {
+//     event.preventDefault()
+//     city = inputEl.value
   
-     if (!inputEl) {
-      console.error('You need to search for a city!');
-       return;
-     }
+//      if (!inputEl) {
+//       console.error('You need to search for a city!');
+//        return;
+//      }
   
-    var queryString = './landing.html?q=' + city
+//     var queryString = './landing.html?q=' + city
   
-    location.assign(queryString)
+//     location.assign(queryString)
     
-  }
-  buttonEl.addEventListener('click', pageChange)
+//   }
+//   buttonEl.addEventListener('click', pageChange)
 
 
 
 
 
 
-document.getElementById("submit").addEventListener("click", getRepo)
+ document.getElementById("button").addEventListener("click", getRepo)
