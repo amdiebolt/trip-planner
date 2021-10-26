54ad0eb8c16edec9053b174066043480dbf103d1
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2021-10-25 18:55:36 -0700

    update script2
    updating some variables and starting on fetch function

--- script2.js
+++ script2.js
@@ -3,27 +3,27 @@ var ticketMasterCont = document.querySelector("#ticketCont");
 var weatherCont = document.querySelector("#weatherCont")
 
 
-
-var weatherURL = "https://api.openweathermap.org/"
-var weatherAPIKey = "966a86c8bd69d14a621d45a4cd70fed2"
-var ticketmasterURL = "https://app.ticketmaster.com/discovery/v2/events?"
-var ticketmasterAPIKey ="apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0"
-
-
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+var wRootURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+var wAPIKey = "&appid=966a86c8bd69d14a621d45a4cd70fed2"
+var tRootURL = "https://app.ticketmaster.com/discovery/v2/events?"
+var tAPIKey ="apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0"
 
 
 
 
 function getWeatherAPI(city){
+    wURL = wRootURL + city + wAPIKey
+    console.log(wURL)
 
-
-
-    fetch()
+    fetch(wURL)
         .then(function(response){
         return response.json()
         })
     
         .then(function(data){
+            var wdata = data[0]
+            console.log(wdata)
         
         })
 
@@ -36,7 +36,6 @@ function getLocalValue(){
     console.log(myStorage)
     let cityName = myStorage.cityName
     console.log(cityName)
-
 
     getWeatherAPI(cityName)
 }
