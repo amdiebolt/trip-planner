1c7225ae5f83038e629bff47d17f2337495ec2b8
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2021-10-27 13:35:06 -0700

    Added notes to JS
    Deleted some excess code. Added some comments, and made structure easier to read.

--- script2.js
+++ script2.js
@@ -3,12 +3,11 @@ var title = document.querySelector("#titleHeader")
 var ticketMasterCont = document.querySelector(".ticketCont")
 var weatherCont = document.querySelector(".weatherCont")
 var dateTitle = document.querySelector(".showDate")
-// var weatherBlock = document.querySelector(".weatherBlock")
 
-
+// key to be used in both openWeatherMap APIs
 var wAPIKey = "&appid=966a86c8bd69d14a621d45a4cd70fed2"
 
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},
 // {country code}&limit={limit}&appid={API key}
 var wGeoRootURL = "https://api.openweathermap.org/geo/1.0/direct?q="
 var paramGeo =",US&limit=5"
@@ -18,7 +17,6 @@ var wRootURL = "https://api.openweathermap.org/data/2.5/forecast?lat="
 var lon = "&lon="
 var paramWeath = "&units=imperial"
 
-
 //https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0
 //&radius=30&unit=miles&source=ticketmaster&locale=*&endDateTime=2021-10-29T19:35:00Z&size=5&city=los%20angeles&stateCode=CA&preferredCountry=us
 var tRootURL = "https://app.ticketmaster.com/discovery/v2/events?"
@@ -26,37 +24,37 @@ var tAPIKey ="&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0"
 
 
 function getWeatherAPI(city){
-    // weatherCont.innerHTML = ""
-    //ticketMasterCont.innerHTML = ""
     
-
-
     // testing data and grabbing latitude and longitude
     var wURL = wGeoRootURL +  city + paramGeo + wAPIKey
 
+    //
     fetch(wURL)
         .then(function(response){
         return response.json()
         })
     
+        //
         .then(function(data){
             var geodata = data[0]
-        
             var w5dayURL = wRootURL + geodata.lat + lon + geodata.lon + paramWeath + wAPIKey
 
+            //
             fetch(w5dayURL)
                 .then(function(response){
                     return response.json()
                 })
+                // 
                 .then(function(wData){
                     var cityData = wData
                     // console.log(cityData)
                 
+                    // Create header element, set value to city + todays date, and add to top of page
                     var cityTitle = document.createElement("h1")
                     cityTitle.textContent = city.toUpperCase() + " " + moment.unix(wData.city.sunrise).format("DD/MM/YYYY")
                     title.prepend(cityTitle)
 
-                    // I think this needs to be in a loop
+                    // 
                     for(var i = 0; i < wData.list.length; i=i+8){
                         var thisDate = wData.list[i]
                         // Set up the daily weather containers in enirety
@@ -65,15 +63,16 @@ function getWeatherAPI(city){
                         wCardCol.className = "card"
                         wCardCol.setAttribute("style", "width: 300px;")
                         
+                        // Should I add the icon for the daily weather?
                         var todayDate = document.createElement("div")
                         todayDate.className = "card-divider align-center"
                         todayDate.textContent = moment.unix(thisDate.dt).format("DD-MM-YYYY")
                         wCardCol.append(todayDate)
-                        // moment.unix(thisDate.dt).format("DD-MM-YYYY")
+                        
                         
                         var wBlock = document.createElement("div")
                         wBlock.className = "card-section weatherBlock"
-                        // Should I add the icon for the daily weather?
+                        
 
                         var wList = document.createElement("div")
                         wList.className = "card-section weatherBlock"
@@ -105,6 +104,8 @@ function getWeatherAPI(city){
 
                         console.log(thisDate)
                         console.log(i)
+
+                        //Create another fetch to grab and show data from ticketmaster
                     
                     }
                 })
